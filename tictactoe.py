2e27c4f8f3666cdea9cb03249c1da264041f669a
konseptgrokker <oliver.bergqvist@gmail.com>
2017-12-25 16:08:52 +0100

    Add has_won() function and change symbol for empty slot to None instead of 0

--- tictactoe.py
+++ tictactoe.py
@@ -1,4 +1,4 @@
-def get_board(matrix):
+def draw_board(matrix):
     # Initialize board (This string will be the one returned)
     board = ""
     # Iterate over all slots
@@ -6,13 +6,13 @@ def get_board(matrix):
         # Add a row
         for slot_index, slot in enumerate(row):
             # Add X's and O's aaccording to matrix
-            board += (" " * 5 if slot == 0 else
+            board += (" " * 5 if slot == None else
                       ("  x  " if slot == 1 else
                        "  o  "))
-            # Add veritcal line, except after the last slot (slot 3)
+            # Add veritcal line, except after the last slot (slot 2)
             if slot_index < 2:
                 board += "|"
-        # Add line, except under last row (row 3)
+        # Add line, except under last row (row 2)
         if row_index < 2:
             board += f"\n{'-' * 17}\n"
     return board
@@ -21,15 +21,40 @@ def get_board(matrix):
 def make_move(matrix, piece, x, y):
     # In matrices the y-axis goes down, so we subtract it from the height
     # Subtract 1 from x to start from 1 and not 0
-    if matrix[3 - y][x - 1] == 0:
+    if matrix[3 - y][x - 1] == None:
         matrix[3 - y][x - 1] = piece
     return matrix
 
-# def has_won(matrix, piece)
+def has_won(matrix):
+    # Check the rows
+    for row in matrix:
+        if row == [1, 1, 1]:
+            return 1
+        if row == [2, 2, 2]:
+            return 2
+    # Check the columns (Iterate over transpose matrix)
+    for column in zip(*matrix):
+        # zip turns the lists into tuples
+        if column == (1, 1, 1):
+            return 1
+        if column == (2, 2, 2):
+            return 2
+    # Check the diagonals
+    if ((matrix[0][0] == 1 and matrix[2][2] == 1) or \
+    (matrix[0][2] == 1 and matrix[2][0] == 1)) \
+      and matrix[1][1] == 1:
+        return 1
+    if ((matrix[0][0] == 2 and matrix[2][2] == 2) or \
+    (matrix[0][2] == 2 and matrix[2][0] == 2)) \
+      and matrix[1][1] == 2:
+        return 2
+
+
+
 
 if __name__ == "__main__":
-    matrix = [[0, 0, 0] for _ in range(3)]
-    print(get_board(matrix), matrix)
-    matrix = make_move(matrix, 2, 3, 3)
-    print(get_board(matrix))
-    print(matrix)
+    print(has_won([
+        [2, None, 1],
+        [2, 1, None],
+        [1, 1, 2]
+    ]))
